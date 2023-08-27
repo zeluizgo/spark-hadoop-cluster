82bf4a754d3c5d63c9c8db16a6ec37df3a1dfe4b
Jose Luiz Clemente Goncalves <joseluizcg@hotmail.com>
2023-08-27 16:41:25 -0300

    ajkueste ce xei la

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 docker build -t spark-hadoop-cluster ./spark-hadoop
 docker build -t hive-server -f ./hive/Dockerfile-hive ./
 docker build -t jupyter -f ./jupyter/Dockerfile-jupyter ./
+docker stack deploy --compose-file=docker-compose.yml spark-hadoop
