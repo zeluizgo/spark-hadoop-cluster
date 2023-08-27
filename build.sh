70fdfd0bd201e3b7a7869cceecd09c0dda392d97
Jose Luiz Clemente Goncalves <joseluizcg@hotmail.com>
2023-08-27 09:55:42 -0300

    ajustes de deduplicação de arquivos

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker build -t spark-hadoop-cluster ./spark-hadoop
-docker build -t hive-server ./hive
-docker build -t jupyter ./jupyter
+docker build -t hive-server -f ./hive/Dockerfile-hive ./
+docker build -t jupyter -f ./jupyter/Dockerfile-jupyter ./
