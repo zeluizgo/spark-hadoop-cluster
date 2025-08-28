dfe6a0920e7fc02f4d2e5c621301585f9267fafc
Jose Luiz Clemente Goncalves <joseluizcg@hotmail.com>
2025-08-28 00:17:58 -0300

    refatora a criação do banco de dados metastore para verificar a existência antes de criar

--- hive/scripts/bootstrap.sh
+++ hive/scripts/bootstrap.sh
@@ -14,14 +14,21 @@ service mariadb start
 #mysqld --user=mysql
 
 # Configs de Hive, configurando o metastore, definindo senha, etc...
-mysql -u root -Bse \
-"CREATE DATABASE metastore; \
-USE metastore; \
-SOURCE /usr/hive/scripts/metastore/upgrade/mysql/hive-schema-3.1.0.mysql.sql; \
-CREATE USER 'hive'@'localhost' IDENTIFIED BY 'password'; \
-REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'hive'@'localhost'; \
-GRANT ALL PRIVILEGES ON metastore.* TO 'hive'@'localhost'; \
-FLUSH PRIVILEGES;"
+DB_EXISTS=$(mysql -u root -Bse "SHOW DATABASES LIKE 'metastore';")
+
+if [ -z "$DB_EXISTS" ]; then
+    echo "Metastore database does not exist. Creating and initializing..."
+    mysql -u root -Bse "
+    CREATE DATABASE metastore;
+    USE metastore;
+    SOURCE /usr/hive/scripts/metastore/upgrade/mysql/hive-schema-3.1.0.mysql.sql;
+    CREATE USER 'hive'@'localhost' IDENTIFIED BY 'password';
+    REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'hive'@'localhost';
+    GRANT ALL PRIVILEGES ON metastore.* TO 'hive'@'localhost';
+    FLUSH PRIVILEGES;"
+else
+    echo "Metastore database already exists. Skipping creation."
+fi
 
 #Persistindo dados anteriores do metastore
 mysql metastore < /hadoop_data/dump/metastore_dump
