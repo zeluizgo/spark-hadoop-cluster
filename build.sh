ef602e4e7c7b5a068e67beb998f04bd16aa3eadc
Jose Luiz Clemente Goncalves <joseluizcg@hotmail.com>
2026-02-24 23:25:30 -0300

    atualiza configuração do Docker para usar repositório local e remove arquivos do Hive

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
-docker build -t spark-hadoop-cluster ./spark-hadoop
-#docker build -t hive-server -f ./hive/Dockerfile-hive ./
-#docker build -t jupyter -f ./jupyter/Dockerfile-jupyter ./
+
+docker build --platform linux/arm64 -t spark-hadoop-cluster ./spark-hadoop
+docker tag spark-hadoop-cluster ubuntu-pi-03:5000/spark-hadoop-cluster
+docker push ubuntu-pi-03:5000/spark-hadoop-cluster
+
+
+#docker build --platform linux/arm64 -t jupyter -f ./jupyter/Dockerfile-jupyter ./jupyter
 #docker stack deploy --compose-file=docker-compose.yml spark-hadoop
