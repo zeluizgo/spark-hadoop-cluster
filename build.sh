ffe37a98783fafd6af5ba811dcdc02a98a628f11
Jose Luiz Clemente Goncalves <joseluizcg@hotmail.com>
2026-02-25 00:39:06 -0300

    adiciona configuração de volume para DataNode no docker-compose

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ docker tag spark-hadoop-cluster ubuntu-pi-03:5000/spark-hadoop-cluster
 docker push ubuntu-pi-03:5000/spark-hadoop-cluster
 
 
-#docker build --platform linux/arm64 -t jupyter -f ./jupyter/Dockerfile-jupyter ./jupyter
-#docker stack deploy --compose-file=docker-compose.yml spark-hadoop
+docker build --platform linux/arm64 -t jupyter -f ./jupyter/Dockerfile-jupyter ./
+docker tag jupyter ubuntu-pi-03:5000/jupyter
+docker push ubuntu-pi-03:5000/jupyter
+#docker stack deploy --compose-file=docker-compose.yml spark-hadoop     
