0c2816bcb8995e0273540802223d0a1f41e5aaa5
Jose Luiz Clemente Goncalves <joseluizcg@hotmail.com>
2026-04-22 18:28:25 -0300

    altera o Dockerfile para remover a especificação de plataforma e ajusta o script de build para usar buildx

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 
-docker build --platform linux/arm64 -t spark-hadoop-cluster ./spark-hadoop
+# Requires a buildx builder with arm64 support.
+# One-time setup: docker buildx create --name arm64builder --use && docker buildx inspect --bootstrap
+
+docker buildx build --platform linux/arm64 --load -t spark-hadoop-cluster ./spark-hadoop
 docker tag spark-hadoop-cluster ubuntu-pi-03:5000/spark-hadoop-cluster
 docker push ubuntu-pi-03:5000/spark-hadoop-cluster
 
-
-docker build --platform linux/arm64 -t jupyter -f ./jupyter/Dockerfile-jupyter ./
+docker buildx build --platform linux/arm64 --load -t jupyter -f ./jupyter/Dockerfile-jupyter ./
 docker tag jupyter ubuntu-pi-03:5000/jupyter
 docker push ubuntu-pi-03:5000/jupyter
 
-docker build --platform linux/arm64 -t hive -f ./hive/Dockerfile-hive ./
+docker buildx build --platform linux/arm64 --load -t hive -f ./hive/Dockerfile-hive ./
 docker tag hive ubuntu-pi-03:5000/hive
 docker push ubuntu-pi-03:5000/hive
-#docker stack deploy --compose-file=docker-compose.yml spark-hadoop     
+#docker stack deploy --compose-file=docker-compose.yml spark-hadoop
